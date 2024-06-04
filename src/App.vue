ed5126de61f37d788e3d0ff6f0b0151d934d4662
David Kimani <daviemim07@gmail.com>
2024-06-04 21:33:49 +0300

    options API

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,27 @@
-<script setup>
-const name = 'Kimani';
-
-const options = { href: 'https://google.com', target: '_blank' };
-
-function capitalizeName(name) {
-  name.toUpperCase();
-}
-
-const loggedIn = false;
-const user = undefined;
-
-// document.querySelector('#form').addEventListener('submit', (e) => {
-//   e.preventDefault();
-// });
-
-function login() {
-  console.log('You are now logged in');
-}
+<script>
+// options Api
+export default {
+  data: () => ({
+    bankBalance: 100,
+    name: 'Kimani',
+    nums: [1, 2, 3, 4]
+  }),
+  methods: {
+    increaseBalance() {
+      this.bankBalance += 20;
+    }
+  },
+  mounted() {
+    // do something when the component is rendered
+  }
+};
 </script>
 
 <template>
-  <h1>This is Vue js session taken by {{ capitalizeName(name) }}</h1>
-  <a v-bind="options">Go to google</a>
-  <br />
-  <button>Understand Vue Js</button>
+  <div>
+    <h1>Reactivity</h1>
 
-  <h2 v-if="loggedIn">Sensitive information {{ user?.sensitivieInformation }}</h2>
-  <h2 v-else-if="!user">User is not defined</h2>
-  <h2 v-else>Something else</h2>
-
-  <form action="" @submit.prevent="login">
-    <input type="text" placeholder="Full Name" />
-    <input type="text" placeholder="Email" />
-    <input type="submit" value="Submit" />
-  </form>
+    <p>{{ bankBalance }}</p>
+    <button @click="increaseBalance">Pay Me!!</button>
+  </div>
 </template>
