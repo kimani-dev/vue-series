a30471d4b0d4bacf73710f4c8f8a6d26f2b69d2d
David Kimani <daviemim07@gmail.com>
2024-06-06 22:25:49 +0300

    list rendering

--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,40 @@
 <script setup>
-// Composition API
-import { ref, computed, onMounted } from 'vue';
-
-const bankBalance = ref(100);
-const name = ref('Kimani');
-
-// note: whatever you pass in ref is the initial value of that variable
-// note: when you pass a reactive value inside another ref, it is unwrapped
-const balanceInKES = computed(() => bankBalance.value * 130.5);
-const statement = computed(() => `${name.value}'s bank balance is USD ${bankBalance.value}`);
-
-function increaseBalance() {
-  bankBalance.value += 20;
-}
-
-function updateName(first, last) {
-  name.value = `${first} ${last}`;
-}
-
-// vue depends on Javascript proxies to handle reactivity
-const person = {
-  name: 'Kimani',
-  age: 55
-};
-
-let proxy = new Proxy(person, {
-  get() {
-    // update the DOM
-    console.log('We are accessing a property in the person object');
-    return 'Different thing';
+// List rendering
+const users = [
+  {
+    name: 'Mirriam Njeri',
+    email: 'mirriam@gmail.com'
   },
-  set() {
-    // update the DOM
-    // return new value
-    return '';
+  {
+    name: 'Brian Kimani',
+    email: 'brian@gmail.com'
+  },
+  {
+    name: 'David Maina',
+    email: 'david@gmail.com'
   }
-});
-
-console.log(proxy.name);
-
-onMounted(() => {
-  console.log('The component has been mounted');
-});
+];
 </script>
 
 <template>
   <div>
-    <h1>Reactivity</h1>
-    <!-- State unwrapping -->
-    <p>USD: {{ bankBalance }}</p>
-    <p>KES: {{ balanceInKES }}</p>
+    <h1>List Rendering</h1>
 
-    <h5>{{ statement }}</h5>
-    <button @click="increaseBalance">Pay Me!!</button>
-    <button @click="updateName('David', 'Kimani')">Update Name</button>
+    <!-- destructuring a v-for -->
+    <div class="user" v-for="({ name, email }, index) in users" :key="email">
+      {{ `User number ${index + 1}` }}
+      <p>{{ name }}</p>
+      <p>{{ email }}</p>
+    </div>
   </div>
 </template>
+
+<style scoped>
+.user {
+  background-color: gray;
+  color: white;
+  padding: 2px;
+  padding-left: 10px;
+  margin-top: 10px;
+}
+</style>
