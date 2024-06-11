c5c4888547aa98780d6c7d8a4de32d8928ea2389
David Kimani <daviemim07@gmail.com>
2024-06-11 22:26:10 +0300

    lifecycle-hooks

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,29 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, onMounted, onBeforeUnmount } from 'vue';
 
-const user = ref({
-  email: null,
-  password: null
+const school = ref('');
+
+const interval = setInterval(() => {
+  console.log('I run every second');
+}, 1000);
+
+onMounted(() => {
+  // this callback will run when the component has been mounted to the DOM
+  const schoolInput = document.getElementById('school');
+  schoolInput.addEventListener('change', (e) => {
+    school.value = e.target.value;
+  });
 });
 
-const isLoggedIn = ref(false);
-const emailVerified = ref(false);
-
-function login() {
-  if (user.value.email && user.value.password) {
-    isLoggedIn.value = true;
-    return;
-  }
-  alert('Please fill in all the fields');
-}
-
-function verifyEmail() {
-  emailVerified.value = true;
-}
+onBeforeUnmount(() => clearInterval(interval));
 </script>
+
 <template>
   <div>
-    {{ isLoggedIn }}
-    <h1>Conditional Rendering</h1>
-    <form @submit.prevent="login" v-if="!isLoggedIn">
-      <input type="email" placeholder="Email Address" v-model="user.email" /> <br />
-      <input type="password" placeholder="Password" v-model="user.password" /> <br />
-      <input type="submit" value="Login" />
-    </form>
+    <h1>Lifecycle Hooks</h1>
 
-    <!-- email verification -->
-    <div v-else-if="!emailVerified">
-      <h2>Your email is not verified. Verify your email to continue</h2>
-      <button @click="verifyEmail">Verify email</button>
-    </div>
-
-    <!-- Dashboard -->
-    <h2 v-else>Welcome to your dashboard</h2>
+    {{ school }}
+    <!-- input with a lazy modifier -->
+    <input type="text" id="school" placeholder="School" />
   </div>
 </template>
